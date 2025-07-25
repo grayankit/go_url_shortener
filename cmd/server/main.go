fd4194e4ab5e9e5a291113361844fae49d526295
Ankit Grai <ankigray@outlook.com>
2025-07-25 15:34:39 +0530

    feat: added logging for every request

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 
 	"github.com/grayankit/go_url_shortener/internal/handler"
+	"github.com/grayankit/go_url_shortener/internal/logger"
+	"github.com/grayankit/go_url_shortener/internal/middleware"
 )
 
 func main() {
@@ -13,8 +15,13 @@ func main() {
 	mux.HandleFunc("/shorten", handler.Shorten)
 	mux.HandleFunc("/u/", handler.Redirect)
 
+	//Logging every request
+	logger := logger.NewLogger()
+	loggingMiddleware := middleware.LoggingMiddleware(logger)
+	wrappedMux := loggingMiddleware(mux)
+
 	log.Println("Starting server on :8080")
-	err := http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(":8080", wrappedMux)
 
 	if err != nil {
 		log.Fatal(err)
